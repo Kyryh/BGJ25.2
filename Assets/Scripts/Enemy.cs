1339374f23d55e95100a6564c65b3401d58bf93c
Kyryh <71778401+Kyryh@users.noreply.github.com>
2025-08-26 20:39:59 +0200

    Make enemies target and move towards the oven

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+    Rigidbody2D rb;
+    [SerializeField]
+    float speed;
+    Vector3 GetTarget() {
+        return Oven.Instance.transform.position;
+    }
     void Awake() {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
 
+    }
+
+    private void FixedUpdate() {
+        var direction = GetTarget() - transform.position;
+        direction.Normalize();
+        rb.linearVelocity = direction * speed;
     }
 
     private void OnTriggerStay2D(Collider2D collision) {
